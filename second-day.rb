bd17ccc78662d45b5c823ab02f1c850b641132d9
christophebrucker <christophe.brucker@gmail.com>
2021-10-27 13:51:05 +0200

    Second Day

---
+++ second-day.rb
@@ -0,0 +1,42 @@
+#This sis the second day we practice Ruby for the toptal test
+=begin
+  This is a multine comment.
+  Everything between line 2 and 5 is commentted out.
+=end
+
+#print and puts
+# print somehting
+print "Hello World printed out with print"
+
+# print something and add a line after
+puts "Hi Chris printed out with puts"
+print "Hello World printed out with print"
+
+# arithmetic operations in Ruby
+# addition outpout 4
+print 1+3
+
+# substraction output -1
+print 8-9
+
+# division output 3
+print 24/8
+
+# multiplication output 4
+print 24/6
+
+# exponentiation output 27
+print 3**3
+
+# modulo output 2
+print 24%11
+
+# numeric data types
+# Integer value
+x = 10
+
+# Float Value
+y = 10.2
+
+
+
